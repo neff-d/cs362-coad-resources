2edaa62bc254d8cae2bd90263683388de5f70f3f
Daniel Neff <neffd@oregonstate.edu>
2023-01-17 20:40:30 -0800

    Added 2 more tests to resource_category_spec.rb for validating name length

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe ResourceCategory, type: :model do
 
     it {should have_and_belong_to_many(:organizations).class_name('Organization')}
     it {should have_many(:tickets).class_name('Ticket')}
+    
+    it "should be less than 255 characters long" do
+        r = ResourceCategory.new
+        r.name = "i" * 256
+        expect(r).to_not be_valid
+    end
+    it "should have at least 1 character" do
+        r = ResourceCategory.new
+        r.name = "i" * 0
+        expect(r).to_not be_valid
+    end
 end
