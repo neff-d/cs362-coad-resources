b7d1aa0c9bf91e17c49bb3a7d329b6b2418ab77a
Tristan Hook <tristanskyhook@gmail.com>
2023-01-21 16:53:09 -0800

    advanced test for user sign in

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
 
+    before do
+        @user = User.create!(email: 'example@example.com', password: 'password', password_confirmation: 'password')
+    end
+
+    it 'approves an organization' do
+
+        visit new_user_session_path
+        fill_in 'Email', with: @user.email
+        fill_in 'Password', with: @user.password
+        click_button 'Sign in'
+
+        expect(page).to have_content('You have to confirm your email address before continuing.')
+
+    end
+
+
 end
--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
 
+    it 'allows a user to register' do
+        visit '/users/sign_up'
+        fill_in 'Email', with: 'example@example.com'
+        fill_in 'Password', with: 'password'
+        fill_in 'Password confirmation', with: 'password'
+        click_button 'Sign up'
+
+        expect(page).to have_content('A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
+
+    end
+
 end
