2a2c914423f799026bd68eed8778c833915a5881
Tristan Hook <hookt@oregonstate.edu>
2023-02-02 11:59:58 -0800

    titket test

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Ticket, type: :model do
     it {should belong_to(:region).class_name('Region')}
     it {should belong_to(:organization).class_name('Organization')}
     it {should belong_to(:resource_category).class_name('ResourceCategory')}
+    
 
      
-
 end
