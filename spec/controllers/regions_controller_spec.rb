0d471ae784b8f31dd1ec79200e2a53cfe0834f5a
Daniel Neff <neffd@oregonstate.edu>
2023-02-16 11:48:09 -0800

    some region controller tests added

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -17,6 +17,56 @@ RSpec.describe RegionsController, type: :controller do
         expect(rc.index).to be_a(ActiveRecord::Relation)
     end
 
+    context 'as a logged-in user' do
+        let(:user) { create(:defaultUser) }
+        before(:each) { sign_in(user) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to redirect_to(dashboard_path) }
+        end
+
+        describe "GET #show" do
+            let(:region) { create(:region) }
+            it { expect(get(:show, params: { id: region.id } )).to redirect_to(dashboard_path) }
+        end
+
+     end
+
+    context 'as a logged-out user' do
+        let(:user) { create(:defaultUser) }
+        
+        describe "GET #index" do
+            it { expect(get(:index)).to redirect_to(new_user_session_path) }
+        end
+
+    end
+
+    context 'as an admin' do
+        let(:user) { create(:defaultUser, :admin) }
+        before(:each) { sign_in(user) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to be_successful }
+        end
+
+        describe "GET #show" do
+            let(:region) { create(:region) }
+            it { expect(get(:show, params: { id: region.id } )).to be_successful }
+        end
+
+        describe "POST #create" do
+            let(:region) { create(:region) }
+
+            it {
+                # r = build(:region)
+                # post(:create, params: { region: attributes_for(:region) })
+                # expect(respone).to be_successful
+            }
+            
+        end
+    end
+
+
     
 
     
