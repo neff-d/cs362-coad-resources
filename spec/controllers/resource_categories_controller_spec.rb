96f207ab8c2994ce9f5f1917bc0f80308e5c15f9
Daniel Neff <neffd@oregonstate.edu>
2023-02-23 11:52:39 -0800

    more tests for resource_categories controllers

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -16,4 +16,50 @@ RSpec.describe ResourceCategoriesController, type: :controller do
         rcc = ResourceCategoriesController.new
         expect(rcc.index).to be_a(ActiveRecord::Relation)
     end
+
+    context "as a logged-out user" do
+        let(:user) { create(:defaultUser) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to redirect_to(new_user_session_path) }
+        end
+    end
+
+    context "as a logged-in user" do
+        let(:user) { create(:defaultUser) }
+        before(:each) { sign_in(user) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to redirect_to(dashboard_path)}
+        end
+    end
+    
+    context "as an admin" do
+        let(:user) { create(:defaultUser, :admin) }
+        before(:each) { sign_in(user) }
+
+        describe "GET #index" do
+            it { expect(get(:index)).to be_successful}
+        end
+
+        describe "GET #new" do
+            it { expect(get(:new)).to be_successful}
+        end
+
+        describe "POST #create" do
+
+        it {
+            post(:create, params: { resource_category: attributes_for(:resource_category) })
+            expect(response).to redirect_to(resource_categories_path)
+        }
+
+        it {
+            expect_any_instance_of(ResourceCategory).to receive(:save).and_return(false)
+
+            post(:create, params: { resource_category: attributes_for(:resource_category) })
+            expect(response).to be_successful
+        }
+
+        end
+    end
 end
