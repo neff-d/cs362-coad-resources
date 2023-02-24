a5f948c7194ef664ba8c9793176cc8f6a10f0ee8
Daniel Neff <neffd@oregonstate.edu>
2023-02-23 16:32:05 -0800

    now 100% on resource_category_controller spec

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -48,18 +48,83 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
         describe "POST #create" do
 
-        it {
-            post(:create, params: { resource_category: attributes_for(:resource_category) })
-            expect(response).to redirect_to(resource_categories_path)
-        }
+            it {
+                post(:create, params: { resource_category: attributes_for(:resource_category) })
+                expect(response).to redirect_to(resource_categories_path)
+            }
 
-        it {
-            expect_any_instance_of(ResourceCategory).to receive(:save).and_return(false)
+            it {
+                expect_any_instance_of(ResourceCategory).to receive(:save).and_return(false)
 
-            post(:create, params: { resource_category: attributes_for(:resource_category) })
-            expect(response).to be_successful
-        }
+                post(:create, params: { resource_category: attributes_for(:resource_category) })
+                expect(response).to be_successful
+            }
+        end
 
+        describe "PATCH #update" do
+
+            let(:resource_category) { create(:resource_category) }
+
+            it {
+                expect(patch(:update, params: { id: resource_category.id, resource_category: attributes_for(:resource_category) } )).to redirect_to(@resource_category)
+            }
+        end
+
+        describe "PUT #update" do
+
+            let(:resource_category) { create(:resource_category) }
+
+            it {
+                expect_any_instance_of(ResourceCategory).to receive(:update).and_return(false)
+                expect(patch(:update, params: { id: resource_category.id, resource_category: attributes_for(:resource_category) } )).to be_successful
+            }
+
+        end
+
+        describe "PATCH #activate" do
+
+            let(:resource_category) { create(:resource_category) }
+
+            it { 
+                patch(:activate, params: { id: resource_category.id } )
+                expect(:notice).to eq(:notice) { 'Category activated.' }
+            }
+
+            it {
+                expect_any_instance_of(ResourceCategory).to receive(:activate).and_return(false)
+
+                patch(:activate, params: { id: resource_category.id } )
+                expect(:alert).to eq(:alert) { 'There was a problem activating the category.'}
+            }
+
+        end
+
+        describe "PATCH #deactivate" do
+
+            let(:resource_category) { create(:resource_category) }
+
+            it {
+                patch(:deactivate, params: { id: resource_category.id } )
+                expect(:notice).to eq(:notice) { 'Category deactivated.' }
+            }
+
+            it {
+                expect_any_instance_of(ResourceCategory).to receive(:deactivate).and_return(false)
+
+                patch(:deactivate, params: { id: resource_category.id } )
+                expect(:alert).to eq(:alert) { 'There was a problem deactivating the category.' }
+            }
+
+        end
+
+        describe "DELETE #destroy" do
+
+            let(:resource_category) { create(:resource_category) }
+
+            it {
+                delete(:destroy, params: { id: resource_category.id } )
+                expect(response).to redirect_to(resource_categories_path)
+            }
         end
     end
 end
