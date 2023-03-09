28e5d9d2aebb87d937cf802a5939fb3999acd97e
Tristan <tristanskyhook@gmail.com>
2023-03-09 11:33:31 -0800

    user_mailers

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe UserMailer, type: :mailer do
         expect(UserMailer).to be_a(Class)
     end
 
+    it 'sends an email' do
+        params = { to: 'example@example.com', new_organization: Organization.new }
+        email = UserMailer.with(params).new_organization_application
+        expect(email).to have_content params[:to]
+    end
+
+    it 'cannot send an email in development' do
+        #set environment to development
+        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development'))
+        params = { to: 'example@example.com', new_organization: Organization.new }
+        email = UserMailer.with(params).new_organization_application
+        expect(email).to have_content ''
+    end
 end
